39f318421adac201f11a319358775dfb2d3121b3
𝓟aͥtriͣcͫ𝒌 <patrickajm@gmail.com>
2026-04-01 15:13:38 -0300

    if

--- 2026_1/Estrutura_de_dados/exercise/segunda_entrega/ListaEncadeada.java
+++ 2026_1/Estrutura_de_dados/exercise/segunda_entrega/ListaEncadeada.java
@@ -25,36 +25,62 @@ armazenar os elementos da lista.
         gasto = 0;
     }
 // 3. Crie um método responsável por inserir um elemento no início da lista.
-    public void inicio(int dado){
+    public void inserir_inicio(int dado){
         if (gasto == limite) AumentarTamanho();
         for (int i = gasto; i > 0; i--){
-            lista[i + 1] = lista[i];
+            lista[i] = lista[i - 1];
         }
         lista[0] = dado;
         gasto ++;
     }
 //4. Crie um método responsável por inserir um elemento no fim da lista.
-    public void fim(int dado){
+    public void remover_fim(int dado){
         if (gasto == limite) AumentarTamanho();
-        lista[gasto + 1] = dado;
+        lista[gasto] = dado;
         gasto ++;
     }
 //5. Crie um método responsável por inserir um elemento em uma posição específica da lista.
-
+    public void inserir_posicao (int dado, int posicao){
+        if (gasto == limite) AumentarTamanho();
+        for (int i = gasto - 1; i > posicao; i--) lista[i + 1] = lista[i];
+        lista[posicao] = dado;
+        gasto ++;
+    }
 //6. Crie um método responsável por remover um elemento no início da lista.
-
+    public void remover_inicio(){
+        for (int i = 0; i < gasto; i++) lista[i] = lista[i + 1];
+        gasto --;
+    }
 //7. Crie um método responsável por remover um elemento no fim da lista.
-
+    public void remover_fim(){
+        gasto --;
+    }
 //8. Crie um método responsável por remover um elemento em uma posição específica da lista.
-
+    public void remover_posicao (int posicao){
+        for (int i = gasto - 1; i > posicao; i--) lista[i - 1] = lista[i];
+        gasto --;
+    }
 //9. Crie um método responsável por remover um elemento específico da lista.
-
+    public void remover_elemento(int elemento){
+       for(int i = 0; i < gasto; i++){
+        if (lista[i] == elemento) remover_posicao(i);
+       }
+       gasto --;
+    }
 //10. Crie um método para exibir o conteúdo de uma lista.
-
+    public void imprimir(){for(int i = 0; i < gasto; i++) System.out.println(lista[i]);}
 /*
 11. Crie um método para pesquisar por um elemento específico em uma lista e
 informar, caso o elemento exista, a posição na qual ele está armazenado.
 */
+    public void pesquisar(int elemento){
+
+        for(int i = 0; i < gasto; i++){
+            if (lista[i] == elemento) {
+                System.out.println("Item encontrado, posicao " + i);
+            }
+        }
+    }
 
 //12. Crie um método que retorne o número de elementos existentes na lista.
 }
