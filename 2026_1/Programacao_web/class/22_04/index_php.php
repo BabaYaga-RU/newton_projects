dffa1335560eaa33fa57b8915added30560e3038
BabaYaga <p201952589703@gmail.com>
2026-04-22 19:57:47 -0300

    22-04

---
+++ 2026_1/Programacao_web/class/22_04/index_php.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Javacript - 22/04</title>
+    </head>
+    <body>
+        <?php
+            echo "Hello World!";
+        ?>
+    </body>
+</html>
