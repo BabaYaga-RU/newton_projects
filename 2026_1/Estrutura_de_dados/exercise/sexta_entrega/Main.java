2644d5e05225653b150942d19a0dc824f4936e48
BabaYaga <p201952589703@gmail.com>
2026-05-07 21:59:22 -0300

    Update Main.java with deadline and exercise details
    Added deadline and exercise list for data structures.

--- 2026_1/Estrutura_de_dados/exercise/sexta_entrega/Main.java
+++ 2026_1/Estrutura_de_dados/exercise/sexta_entrega/Main.java
@@ -1,6 +1,16 @@
-// Prazo: 
+// Prazo: 30/05/2026
 /*
-
+Lista de exercícios 06 – Estruturas de Dados
+1. Faça duas implementações da classe Fila para armazenar valores inteiros, a
+primeira implementação deve utilizar um arranjo para armazenar os elementos
+da estrutura e a segunda implementação de utilizar referências para interligar os
+elementos armazenados. Nas duas estruturas, implemente os itens abaixo:
+2. Crie um método para criar uma fila vazia.
+3. Crie um método responsável por inserir um elemento na fila.
+4. Crie um método responsável por remover um elemento da fila.
+5. Crie um método para exibir o conteúdo de uma fila.
+6. Crie um método que retorne o número de elementos existentes na fila.
+7. Crie um método para verificar se um valor específico está armazenado na fila.
 */
 public class Main {
     public static void main(String[] args) {
