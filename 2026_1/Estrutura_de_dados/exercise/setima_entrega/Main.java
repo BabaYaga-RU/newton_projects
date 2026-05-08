7b312c8166985928d630172c95d0973fd8aaeba8
BabaYaga <p201952589703@gmail.com>
2026-05-07 21:56:11 -0300

    Update Main.java

--- 2026_1/Estrutura_de_dados/exercise/setima_entrega/Main.java
+++ 2026_1/Estrutura_de_dados/exercise/setima_entrega/Main.java
@@ -1,5 +1,13 @@
-// Prazo: 
+// Prazo: 06/06/2026
 /*
+Lista de exercícios 07 – Estruturas de Dados
+1.	Implemente a classe Fila para armazenar valores inteiros, utilizando vetores como estrutura de armazenamento.
+2.	Crie um método para criar uma fila vazia.
+3.	Crie um método responsável por inserir um elemento na fila.
+4.	Crie um método responsável por remover um elemento da fila.
+5.	Crie um método para exibir o conteúdo de uma fila.
+6.	Crie um método que retorne o número de elementos existentes na fila.
+7.	Crie um método para verificar se um valor específico está armazenado na fila.
 
 */
 public class Main {
