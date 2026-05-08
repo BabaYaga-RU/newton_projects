c6c61bd557e5ecdf2df51a31ad1d101765eaafd6
BabaYaga <p201952589703@gmail.com>
2026-05-07 21:58:32 -0300

    Update Main.java

--- 2026_1/Estrutura_de_dados/exercise/quinta_entrega/Main.java
+++ 2026_1/Estrutura_de_dados/exercise/quinta_entrega/Main.java
@@ -1,6 +1,13 @@
-// Prazo: 
+// Prazo: 22/05/2026
 /*
-
+Lista de exercícios 05 – Estruturas de Dados
+1. Implemente a classe Pilha para armazenar valores inteiros, utilizando referências
+para interligar os elementos armazenados.
+2. Crie um método para criar uma pilha vazia.
+3. Crie um método responsável por inserir um elemento na pilha.
+4. Crie um método responsável por remover um elemento da pilha.
+5. Crie um método para exibir o conteúdo de uma pilha.
+6. Crie um método que retorne o número de elementos existentes na pilha.
 */
 public class Main {
     public static void main(String[] args) {
